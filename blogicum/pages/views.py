c783f8f35d90b7e42746caa739d13a64d6de862f
flashikNet <133534020+flashikNet@users.noreply.github.com>
2024-10-31 18:51:41 +0500

    create urls and views

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def about(request):
+    template_name = 'not_implement'
+    return render(request, template_name)
+
+
+def rules(request):
+    template_name = 'not_implement'
+    return render(request, template_name)
